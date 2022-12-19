d77bcf8b6acf916b334bf68a5388e3e4617176c2
ralphHuerta <huertar407@gmail.com>
2022-12-19 07:57:20 -0600

    eureka client

--- admin-cliente/src/main/java/com/admin/cliente/admincliente/AdminClienteApplication.java
+++ admin-cliente/src/main/java/com/admin/cliente/admincliente/AdminClienteApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AdminClienteApplication {
 
 	public static void main(String[] args) {
--- admin-cliente/src/main/java/com/admin/cliente/admincliente/controller/ProductController.java
+++ admin-cliente/src/main/java/com/admin/cliente/admincliente/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.admin.cliente.admincliente.controller;
 import com.admin.cliente.admincliente.model.Produc;
 import com.admin.cliente.admincliente.repository.ProductRep;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,13 +16,24 @@ public class ProductController {
     private ProductRep productRep;
 
     @PostMapping("/crear")
-    public Produc save(@RequestBody Produc produc){
-        return  productRep.save(produc);
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Object> save(@RequestBody Produc produc){
+
+        Produc response = productRep.save(produc);
+        return new ResponseEntity<>( "Producto agregado Exitosamanete con el nombre:" + response.getName(), HttpStatus.CREATED);
     }
 
-    @GetMapping
+    @PutMapping("/actualizar")
+    public ResponseEntity<Object> update(@RequestBody Produc produc){
+
+       Produc response = productRep.save(produc);
+
+       return new ResponseEntity<>("Prodúcto actualizado exitosamente /n" + response.getId(), HttpStatus.CREATED);
+    }
+    @GetMapping("/all")
     public List<Produc> finAll(){
         return productRep.findAll();
     }
+
 }
 
